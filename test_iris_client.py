ec7b4571ebfd74193a5dd46c90ffaff90c63bd13
Maxime Mouchet <max@maxmouchet.com>
2022-01-28 21:52:43 +0100

    tests: test get_all

--- test_iris_client.py
+++ test_iris_client.py
@@ -3,11 +3,10 @@ from iris_client import AsyncIrisClient, IrisClient
 
 def test_client():
     with IrisClient() as client:
-        res = client.get("/users/me")
-        assert res.status_code == 200
+        list(client.get_all("/agents/"))
 
 
 async def test_async_client():
     async with AsyncIrisClient() as client:
-        res = await client.get("/users/me")
-        assert res.status_code == 200
+        async for _ in client.get_all("/agents/"):
+            pass
